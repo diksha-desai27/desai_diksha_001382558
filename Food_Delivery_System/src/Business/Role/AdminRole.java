3b1abd9bd8dc4ea913df8fd37c6ba0a90ce43d4d
Diksha Desai <desai.di@husky.neu.edu>
2020-03-28 16:04:25 -0400

    Working with systemadmin and restaurant admin

---
+++ Food_Delivery_System/src/Business/Role/AdminRole.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Role;
+
+import Business.EcoSystem;
+
+import Business.Organization;
+import Business.Restaurant.Restaurant;
+import Business.UserAccount.UserAccount;
+import userinterface.RestaurantAdminRole.AdminWorkAreaJPanel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author raunak
+ */
+public class AdminRole extends Role{
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Restaurant restaurant, EcoSystem business) {
+        return new AdminWorkAreaJPanel(userProcessContainer, account, restaurant);
+    }
+
+    
+    
+}
---
+++ Food_Delivery_System/src/Business/Role/CustomerRole.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Role;
+
+import Business.EcoSystem;
+
+import Business.Organization;
+import Business.Restaurant.Restaurant;
+import Business.UserAccount.UserAccount;
+import userinterface.CustomerRole.CustomerAreaJPanel;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author raunak
+ */
+public class CustomerRole extends Role{
+
+    
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account,Restaurant restaurant, EcoSystem business) {
+        return new CustomerAreaJPanel(userProcessContainer, account);
+    }
+    
+    
+}
